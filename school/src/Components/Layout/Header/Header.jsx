265299f60b1fe5f0e2d0a90eef5ffbca658c05ce
jeandedieu <jeandedieusessou@gmail.com>
2023-11-10 13:18:00 +0000

    modification du style du header lors du scroll de la page terminé

--- school/src/Components/Layout/Header/Header.jsx
+++ school/src/Components/Layout/Header/Header.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./Header.module.css";
 const Header = () => {
+  const [scroll, setScroll] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 0) {
+        setScroll(true);
+      } else {
+        setScroll(false);
+      }
+    });
+  }, []);
   return (
-    <header className={style.header}>
+    <header
+      className={
+        scroll ? style.header_scroll : style.header
+      } /* onScroll={onScrollHandle} */
+    >
       <section className={style.logo}>
         <div>LOGO</div>
         <div>School site</div>
